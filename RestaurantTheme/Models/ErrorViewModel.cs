c249d59bb428b47c4881c1383073d2c5e4083ce7
Threesquare <Threesquare@DESKTOP-CS2M9PC>
2025-03-05 08:17:40 +0600

    Add project files.

---
+++ RestaurantTheme/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RestaurantTheme.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
